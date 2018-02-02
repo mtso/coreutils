89538e9d121ba69f0f2661d361bc58bc38d25aef
mtso <~@mtso.io>
2018-02-02 00:01:11 -0800

    Handle missing path error

--- src/bin/cat/main.rs
+++ src/bin/cat/main.rs
@@ -3,10 +3,14 @@ extern crate coreutils;
 use std::env;
 use coreutils::cat;
 
-static ARG_ERROR: &'static str = "Need to specify filepath: 'cat path/to/file'";
+static PATH_ERROR: &'static str = "Need to specify filepath: 'cat path/to/file'";
 
 fn main() {
-    let path = env::args().nth(1).expect(ARG_ERROR);
+    let path = match env::args().nth(1) {
+        Some(p) => p,
+        None => return println!("{}", PATH_ERROR),
+    };
+
     match cat(path) {
         Err(e) => println!("{}", e),
         _ => (),
